1844fdc01e41e6d6814add6a0029d5301ef6549a
vatsal-30 <vatsalajmeri30802@gmail.com>
2024-08-22 21:12:10 -0400

    final

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,6 @@ dotenv.config({ path: __dirname + "/config.env" });
 
 const connectDB = async () => {
   try {
-    console.log(__dirname);
 
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
